4c28d904ae3a64f5fdef03a3aaf48107d8a51615
Charly Abraham <charly@core.ai>
2024-08-04 15:46:57 +0530

    feat:initial commit

---
+++ app/src/main/java/prod/phcode/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package prod.phcode.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
